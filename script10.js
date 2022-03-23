3bb85a6f11f6d8701bc656333f985c9d471eb3a5
carolsaint <santocarolina98@gmail.com>
2022-03-22 22:03:36 -0300

    fixed links, imgs, added victory page, finished js in all characters

---
+++ script10.js
@@ -0,0 +1,67 @@
+function choice1(){
+    tries = 2
+    currentTry= 0
+
+    while(tries >= currentTry){
+        var answer = prompt("Choose the correct answer, from 1 to 4")
+    
+        if (answer == '3' && answer != null){
+            alert("Correct!")
+            window.location.assign("./10th-2.html");
+            break
+        } else if (answer != "3"){
+            if (tries > 0) {
+                alert("Try again");
+                tries--;
+                window.location.assign("./gameover.html");
+        }
+        currentTry++
+         }
+    }
+}
+
+
+function choice2(){
+    tries = 2
+    currentTry= 0
+
+    while(tries >= currentTry){
+        var answer = prompt("Choose the correct answer, from 1 to 4")
+    
+        if (answer == '1' && answer != null){
+            alert("Correct!")
+            window.location.assign("./10th-3.html");
+            break
+        } else if (answer != "1"){
+            if (tries > 0) {
+                alert("Try again");
+                tries--;
+                window.location.assign("./gameover.html");
+        }
+        currentTry++
+         }
+    }
+}
+
+function choice3(){
+    tries = 2
+    currentTry= 0
+
+    while(tries >= currentTry){
+        var answer = prompt("Choose the correct answer, from 1 to 4")
+    
+        if (answer == '3' && answer != null){
+            alert("Correct!")
+            window.location.assign("./victory.html");
+            break
+        } else if (answer != "3"){
+            if (tries > 0) {
+                alert("Try again");
+                tries--;
+                window.location.assign("./gameover.html");
+        }
+        currentTry++
+         }
+    }
+}
+
---
+++ script9.js
@@ -0,0 +1,68 @@
+function choice1(){
+    tries = 2
+    currentTry= 0
+
+    while(tries >= currentTry){
+        var answer = prompt("Choose the correct answer, from 1 to 4")
+    
+        if (answer == '4' && answer != null){
+            alert("Correct!")
+            window.location.assign("./9th-2.html");
+            break
+        } else if (answer != "4"){
+            if (tries > 0) {
+                alert("Try again");
+                tries--;
+                window.location.assign("./gameover.html");
+        }
+        currentTry++
+         }
+    }
+}
+
+
+function choice2(){
+    tries = 2
+    currentTry= 0
+
+    while(tries >= currentTry){
+        var answer = prompt("Choose the correct answer, from 1 to 4")
+    
+        if (answer == '2' && answer != null){
+            alert("Correct!")
+            window.location.assign("./9th-3.html");
+            break
+        } else if (answer != "2"){
+            if (tries > 0) {
+                alert("Try again");
+                tries--;
+                window.location.assign("./gameover.html");
+        }
+        currentTry++
+         }
+    }
+}
+
+function choice3(){
+    tries = 2
+    currentTry= 0
+
+    while(tries >= currentTry){
+        var answer = prompt("Choose the correct answer, from 1 to 4")
+    
+        if (answer == '3' && answer != null){
+            alert("Correct!")
+            window.location.assign("./victory.html");
+            break
+        } else if (answer != "3"){
+            if (tries > 0) {
+                alert("Try again");
+                tries--;
+                window.location.assign("./gameover.html");
+        }
+        currentTry++
+         }
+    }
+}
+
+
